384d0c078762dd259638cbed2889ef54565551d8
Uchenna Obicheta <uobicheta@hotmail.com>
2023-01-18 19:32:14 -0500

    Updated index.js with additional prompts to capture user input.

--- index.js
+++ index.js
@@ -22,8 +22,7 @@ inquirer
 		            message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.\n',
 		            name: 'installation',
 		          },
-                  {
-
+        {
                     type: 'input',
                     message: 'Provide instructions and examples for use. Include screenshots as needed.\n To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it.\n Then, using the relative filepath, add it to your README using the following syntax:\n\
                         ```md\n\
@@ -32,6 +31,35 @@ inquirer
                     name: 'usage'
 
                   },
+        {
+                    type: 'input',
+                    message: 'List your collaborators, if any, with links to their GitHub profiles.\nIf you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.\nIf you followed tutorials, include links to those here as well.\n',
+                    name: 'credits'
+
+                  },
+        {
+                    type: 'input',
+                    message: 'The last section of a high-quality README file is the license.\nThis lets other developers know what they can and cannot do with your project.\nIf you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).\n',
+                    name: 'license'
+
+                  },
+        {
+                    type: 'input',
+                    message: 'If your project has a lot of features, list them here.\n',
+                    name: 'features'
+
+                  },
+        {
+                    type: 'input',
+                    message: 'If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so.\n The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you\'d prefer.\n',
+                    name: 'contribute',
+                  },
+        {
+                    type: 'input',
+                    message: 'Go the extra mile and write tests for your application. Then provide examples on how to run them here.\n',
+                    name: 'tests',
+                  },
+
 	    ])
   .then((response) => { 
 	      console.log(response);
