ac9836a89b616536f8b322c9ffbf7a2b89f044da
Uchenna Obicheta <uobicheta@hotmail.com>
2023-01-19 17:42:07 -0500

    Updated README.md file.

---
+++ myREADME.md
@@ -0,0 +1,16 @@
+# asadasd
+## Description
+asdas
+## Installation
+dsadasd
+## Usage
+sadasd
+## Credits
+sadsad
+## Licenseasdad
+## Features
+ sad 
+## Contribute
+asad 
+## Tests
+asd
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,44 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+   
+   let licenseBadge = "";
+
+   if (license) {
+ 
+   }
+
+   return licenseBadge;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+
+  let licenseLink = "";
+
+  if (license) {
+
+  }
+
+  return licenseLink;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+ 
+  let licenseSection = "";
+
+  return licenseSection;
+
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
 
-`;
+
+  return `# ${data.title}\n## Description\n${data.description}\n## Installation\n${data.installation}\n## Usage\n${data.usage}\n## Credits\n${data.credits}\n## License\n${data.license}\n## Features\n ${data.features} \n## Contribute\n${data.contribute} \n## Tests\n${data.tests}`;
 }
 
 module.exports = generateMarkdown;
